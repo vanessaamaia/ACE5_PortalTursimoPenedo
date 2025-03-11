a5f887394936fe978289379f7cc6cb818760cfda
Eversonv4 <eversonv4@gmail.com>
2025-03-11 10:06:10 -0300

    fix: colors in header text in home page

--- assets/js/home/page_active.js
+++ assets/js/home/page_active.js
@@ -1,12 +1,3 @@
 (() => {
   document.querySelector("span.year").textContent = new Date().getFullYear();
-
-  document.querySelectorAll(".nav-links a").forEach((link) => {
-      if (link.href === location.href
-          || link.href === location.href + "#") {
-          link.classList.add("active");
-      } else {
-          link.classList.remove("active");
-      }
-  });
 })()
